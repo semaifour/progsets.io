315a58212dde4eaf0e74b089c028ddb8da86f882
megandran <magendrans@qubercomm.com>
2020-11-25 12:59:19 -0800

    fix settings

--- progsets-psql/src/main/java/io/progsets/rest/GreetingRestController.java
+++ progsets-psql/src/main/java/io/progsets/rest/GreetingRestController.java
@@ -28,8 +28,11 @@ public class GreetingRestController extends BaseController {
 	@GetMapping("/help")
 	public Restponse<Map<String, Object>> help(HttpServletRequest request, HttpServletResponse response) {
 		Map<String, Object> hi = new HashMap<String, Object>();
-		hi.put("name", "progsets-psql service end-point");
-		hi.put("version", "0.0.1");
+		hi.put("Action URL", "$curl -XPOST\n"
+				+ "-H \"Authorization: Basic admin:admin123\" -H\"Content-Type:text/psql\" http://localhost:8175/progesets/rest/psql/exe");
+		hi.put("BODY Content", "credit = ifile?file.path=file:///Users/megandran/Downloads/MyTestData.csv&file.split=true&file.hasheader=true&file.columns=Date:string,Transdate:string,Ref:string,Description:string,Amount:string,Balance:string credit = convert?view=credit&columns=Date:date:mm/dd/yyyy,Transdate:date:mm/dd/yyyy,Ref:string,Description:string,Amount:double,Balance:double result = ssql?sql=select sum(Amount) as Amount from credit return?view=result&as=maps&debug=true close\n"
+				+ "\n"
+				+ "");
 		hi.put("time", new Date());
 		hi.put("status", "running");
 		return new Restponse<Map<String, Object>>(hi);
